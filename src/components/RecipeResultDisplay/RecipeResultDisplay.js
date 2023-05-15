48028e79562b3c1cdf64124b5e0ce8359c66e208
JJ-Bas <jef.j.bas@gmail.com>
2023-05-15 17:13:44 +0200

    RecipeResultDisplay basisfuncties werken, resultaten worden getoond en er kan door de opties worden genavigeerd. Overschrijd de edamamAPI limiet wanneer ik meer dan 1 request doe in de mounting stage van RecipeQuestResult pagina

--- src/components/RecipeResultDisplay/RecipeResultDisplay.js
+++ src/components/RecipeResultDisplay/RecipeResultDisplay.js
@@ -1,20 +1,24 @@
 import React from "react";
+import styles from './RecipeResultDisplay.module.scss'
 
-function RecipeResultDisplay({recipeName, image, time, ingredients}) {
+
+function RecipeResultDisplay({recipeName, image, time, ingredients, back, next}) {
+
     return(
         <>
-            <p> result </p>
-            <div className='outer-display-case'>
-                <button type='button'>next</button>
-                <img url={image}/>
+                        <div className={styles['outer-display-case']}>
+                <button type='button' onClick={next}>next</button>
                 <article>
                     <h2>{recipeName}</h2>
+
+                    <img src={image}/>
+
                     <ul>
                         <li>tijd: {time} minuten</li>
                         <li>aantal ingredienten {ingredients}</li>
                     </ul>
                 </article>
-                <button type='button'>back</button>
+                <button type='button' onClick={back}>back</button>
             </div>
         </>
     )
--- src/pages/RecipeQuestResult/RecipeQuestResult.js
+++ src/pages/RecipeQuestResult/RecipeQuestResult.js
@@ -1,36 +1,71 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import RecipeResultDisplay from "../../components/RecipeResultDisplay/RecipeResultDisplay";
 import axios from "axios";
-
+import {QuestionContext} from "../../Context/QuestionContext/QuestionContext";
 
 
 function RecipeQuestResult() {
 
-    const [recipeList, setRecipeList] = useState({})
+    const [status, setStatus] = useState('pending')
+    const {optionList} = useContext(QuestionContext)
+    const [mainRecipeList, setMainRecipeList] = useState({})
+    const [starterRecipeList, setStarterRecipeList] = useState({})
     const [index, setIndex] = useState(4)
 
-    async function fetchData() {
-        try {
-            const result = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=chicken&app_id=${process.env.REACT_APP_API_ID}&app_key= ${process.env.REACT_APP_API_KEY}`);
-            setRecipeList(result.data);
-        } catch (e) {
-            console.error(e)
+    //useEffect vuurt get request af naar de edamamAPI gebaseerd op de status van de items die uit de QuestionContext worden gehaald
+    useEffect(() => {
+        async function fetchData(mealType, setlist){
+            try {
+                const result = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&beta=true&app_id=${process.env.REACT_APP_API_ID}&app_key= ${process.env.REACT_APP_API_KEY}${mealType}`);
+                setlist(result.data);
+                setStatus('done')
+            } catch (e) {
+                console.error(e)
+            }
+
+        }
+        fetchData('&mealType=Dinner', setMainRecipeList)
+    }, [])
+
+
+    // twee functies om door de resultaten te navigeren. werken onafhankelijk van het aantal hits
+
+    function nextItem() {
+        if (index === (mainRecipeList.hits.length -1)) {
+            setIndex(0)
+        } else {
+            setIndex(index + 1)
+            console.log(index)
+            console.log(mainRecipeList)
+        }
+    }
+
+    function lastItem() {
+        if (index === 0) {
+            setIndex(mainRecipeList.hits.length -1)
+        } else {
+            setIndex(index - 1)
+            console.log(index)
         }
     }
 
 
-
     return (
         <>
+            <button type='button' onClick={() => console.log(mainRecipeList)}>recipeList</button>
+            {status === 'done' ?
 
-            <button type='button' onClick={() => fetchData()}>get data</button>
-            <button type="button" onClick={() => console.log(recipeList)}> console.log</button>
-            <button type='button' onClick={() => console.log(recipeList.hits[4].recipe.label)}>test label</button>
-            <RecipeResultDisplay
-                /*recipeName={recipeList.hits[index].recipe.label}
-                time={recipeList.hits[index].recipe.totalTime}
-                ingredients={recipeList.hits[index].recipe.ingredients.length}*/
-            />
+                <RecipeResultDisplay
+                    next={() => nextItem()}
+                    image={mainRecipeList.hits[index].recipe.image}
+                    recipeName={mainRecipeList.hits[index].recipe.label}
+                    time={mainRecipeList.hits[index].recipe.totalTime}
+                    ingredients={mainRecipeList.hits[index].recipe.ingredients.length}
+                    back={() => lastItem()}
+                /> :
+                <p>Loading...</p>}
+
+
         </>
     )
 }
