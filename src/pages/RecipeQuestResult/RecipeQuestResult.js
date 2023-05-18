d13c9f7c25cabb3eb46a2b6f31a54d62f84f74ef
JJ-Bas <jef.j.bas@gmail.com>
2023-05-18 11:52:51 +0200

    nextItem en lastItem functies in de RecipeQuestResult.js herschreven zodat ze hebruibaar zijn met verschillende state variabelen

--- src/pages/RecipeQuestResult/RecipeQuestResult.js
+++ src/pages/RecipeQuestResult/RecipeQuestResult.js
@@ -2,7 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import RecipeResultDisplay from "../../components/RecipeResultDisplay/RecipeResultDisplay";
 import axios from "axios";
 import {QuestionContext} from "../../Context/QuestionContext/QuestionContext";
-
+import './RecipeQuestResult.scss'
 
 function RecipeQuestResult() {
 
@@ -10,11 +10,13 @@ function RecipeQuestResult() {
     const {optionList} = useContext(QuestionContext)
     const [mainRecipeList, setMainRecipeList] = useState({})
     const [starterRecipeList, setStarterRecipeList] = useState({})
-    const [index, setIndex] = useState(4)
+    const [mainIndex, setMainIndex] = useState(0)
+    const [starterIndex, setStarterIndex] = useState(0)
+    const [dessertIndex, setDessertIndex] = useState(0)
 
     //useEffect vuurt get request af naar de edamamAPI gebaseerd op de status van de items die uit de QuestionContext worden gehaald
     useEffect(() => {
-        async function fetchData(mealType, setlist){
+        async function fetchData(mealType, setlist) {
             try {
                 const result = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&beta=true&app_id=${process.env.REACT_APP_API_ID}&app_key= ${process.env.REACT_APP_API_KEY}${mealType}`);
                 setlist(result.data);
@@ -24,28 +26,29 @@ function RecipeQuestResult() {
             }
 
         }
+
         fetchData('&mealType=Dinner', setMainRecipeList)
     }, [])
 
 
-    // twee functies om door de resultaten te navigeren. werken onafhankelijk van het aantal hits
+    // twee functies om door de resultaten te navigeren. Werken onafhankelijk van het aantal hits
 
-    function nextItem() {
-        if (index === (mainRecipeList.hits.length -1)) {
-            setIndex(0)
+    function nextItem(state, setState) {
+        if (state === (mainRecipeList.hits.length - 1)) {
+            setState(0)
         } else {
-            setIndex(index + 1)
-            console.log(index)
+            setState(state + 1)
+            console.log(state)
             console.log(mainRecipeList)
         }
     }
 
-    function lastItem() {
-        if (index === 0) {
-            setIndex(mainRecipeList.hits.length -1)
+    function lastItem(state, setState) {
+        if (state === 0) {
+            setState(mainRecipeList.hits.length - 1)
         } else {
-            setIndex(index - 1)
-            console.log(index)
+            setState(state - 1)
+            console.log(state)
         }
     }
 
@@ -54,15 +57,32 @@ function RecipeQuestResult() {
         <>
             <button type='button' onClick={() => console.log(mainRecipeList)}>recipeList</button>
             {status === 'done' ?
-
-                <RecipeResultDisplay
-                    next={() => nextItem()}
-                    image={mainRecipeList.hits[index].recipe.image}
-                    recipeName={mainRecipeList.hits[index].recipe.label}
-                    time={mainRecipeList.hits[index].recipe.totalTime}
-                    ingredients={mainRecipeList.hits[index].recipe.ingredients.length}
-                    back={() => lastItem()}
-                /> :
+                <div className='temporary-container'>
+                    <RecipeResultDisplay
+                        next={() => nextItem(starterIndex, setStarterIndex)}
+                        image={mainRecipeList.hits[starterIndex].recipe.image}
+                        recipeName={mainRecipeList.hits[starterIndex].recipe.label}
+                        time={mainRecipeList.hits[starterIndex].recipe.totalTime}
+                        ingredients={mainRecipeList.hits[starterIndex].recipe.ingredients.length}
+                        back={() => lastItem(starterIndex, setStarterIndex)}
+                    />
+                    <RecipeResultDisplay
+                        next={() => nextItem(mainIndex, setMainIndex)}
+                        image={mainRecipeList.hits[mainIndex].recipe.image}
+                        recipeName={mainRecipeList.hits[mainIndex].recipe.label}
+                        time={mainRecipeList.hits[mainIndex].recipe.totalTime}
+                        ingredients={mainRecipeList.hits[mainIndex].recipe.ingredients.length}
+                        back={() => lastItem(mainIndex, setMainIndex)}
+                    />
+                    <RecipeResultDisplay
+                        next={() => nextItem(dessertIndex, setDessertIndex)}
+                        image={mainRecipeList.hits[dessertIndex].recipe.image}
+                        recipeName={mainRecipeList.hits[dessertIndex].recipe.label}
+                        time={mainRecipeList.hits[dessertIndex].recipe.totalTime}
+                        ingredients={mainRecipeList.hits[dessertIndex].recipe.ingredients.length}
+                        back={() => lastItem(dessertIndex, setDessertIndex)}
+                    />
+                </div> :
                 <p>Loading...</p>}
 
 
